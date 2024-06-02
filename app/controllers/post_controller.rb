63195594b611ac135a6734da4e6ec1599e4cb3a4
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-06-02 14:11:06 +0700

    variable fform action to view

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,4 +1,17 @@
 class PostController < ApplicationController
   def index
+
+
+    @firstpost="Example variable from action"
+    @secondpost="Macam macam bahasa pemprograman"
+    @data=[ "1. Pemprograman Ruby", 
+    "2. Pemprograman C++", 
+    "3.Pemprograman JavaScript", 
+    "4. Pemprograman Java", 
+    "5. Pemprograman PHP", 
+    "6. Pemprograman Python", 
+    "7. Pemprograman Perl", 
+    "8. Pemprograman C",
+    ]
   end
 end
