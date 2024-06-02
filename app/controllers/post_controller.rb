d2e4b7c95d672f02697fe48d67095d89fddd4507
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-06-02 14:54:38 +0700

    display all data from sqflite to view

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,17 +1,8 @@
 class PostController < ApplicationController
   def index
+    @data= Post.all
 
 
-    @firstpost="Example variable from action"
-    @secondpost="Macam macam bahasa pemprograman"
-    @data=[ "1. Pemprograman Ruby", 
-    "2. Pemprograman C++", 
-    "3.Pemprograman JavaScript", 
-    "4. Pemprograman Java", 
-    "5. Pemprograman PHP", 
-    "6. Pemprograman Python", 
-    "7. Pemprograman Perl", 
-    "8. Pemprograman C",
-    ]
+   
   end
 end
