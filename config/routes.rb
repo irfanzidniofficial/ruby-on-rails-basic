d9a0da1d2847f059761161bf3eef14996412200a
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-06-02 14:05:01 +0700

    use variable in view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'post/index'
   get '/', to:'home#index'
   get '/index', to:'home#index'
   get "/home", to: "home#index"
   get "/article", to: "home#article"
   get '/about', to: "home#about"
   get '/contact', to: "home#contact"
+  get "/post", to: "post#index"
  
   root "home#index"
 end
